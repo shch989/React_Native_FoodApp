a6ee8d856b4d044553ae9771bc81cb0cd6631bbd
shch989 <tankpop11@gmail.com>
2023-02-06 14:57:05 +0900

    음식 리스트 출력하기

---
+++ components/MealItem.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+function MealItem({ title }) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
+
+export default MealItem
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,26 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { FlatList, View, StyleSheet } from 'react-native'
+
+import { MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0
+  })
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title}/>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   )
 }
